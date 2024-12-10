175b04ed0e9ea22e495dbafaa032c7684ca2c014
Urooj Muhammad Khalid <khalidurooj397@gmail.com>
2024-12-10 08:48:28 +0500

    Update CV

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -47,7 +47,7 @@ const About = () => {
             </p>
             <div className="flex justify-center">
               <button className="inline-flex text-white bg-gray-900 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                <Link href={"assets/cv/cv.jpg"}> View CV </Link>
+                <Link href={"assets/cv/CV.png"}> View CV </Link>
               </button>
             </div>
           </div>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -37,7 +37,7 @@ const Navbar = () => {
               Contact
             </Link>
           </nav>
-          <a href="/assets/cv/cv.jpg">
+          <a href="/assets/cv/CV.png">
             <button className="hover:text-blue-600 inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
               Download CV
               <AiOutlineCloudDownload className="text-xl ml-2 " />
